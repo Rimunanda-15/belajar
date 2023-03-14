22d50db35b9f97de349bae5f02ca01fd3af7f503
Naufal Rizq Ramadhan <naufalrizq100@gmail.com>
2023-03-14 21:33:50 +0700

    JS(#Day1)

---
+++ JS/day-1/1_variables_datatypes/variables.js
@@ -0,0 +1,13 @@
+var pesan = "halo bang"
+var pesan = 123
+pesan = 145
+// var bisa di redeclare dan reassign
+let pesan2 = "ganti pesan"
+// let pesan2 = 123
+pesan2 = 123
+// let gabisa di redeclare tapi bisa di reassign
+
+const phi = 3.14
+phi = 3
+
+console.log(phi)
